09d1c9ed212ba287c65cb1b898fb27842c47b608
AxelShami <123408991+AxelShami@users.noreply.github.com>
2023-03-20 14:17:03 -0400

    Create Compasants.js

---
+++ BackEnd/Compasants.js
@@ -0,0 +1,13 @@
+class Composants {
+   
+    constructor(){
+        var tension;
+    }
+
+    bluedabedi(ababa) {
+        this.courant == ababa++;
+    }
+} 
+const res = new Composants(10,23.2);
+console.log(res.bluedabedi(res.courant))
+
