ab712a86234dd480113252b592c470962515dba1
olivierstKronos <123409452+olivierstKronos@users.noreply.github.com>
2023-03-21 22:43:44 -0400

    Ajout des formules
    Ajouter les formules nécessaire pour certains calcul

--- BackEnd/Composants.js
+++ BackEnd/Composants.js
@@ -6,37 +6,108 @@ class Composant {
         this.courant = courant;
         this.tension = tension;
     }
-    calcul(){
+    calcul(paralele, composant){
         throw console.error();
     }
 } 
 
-class Resistance extends Composant{
-    constructor(circuit, courant, tension, R){
-        super(circuit, courant, tension);
-        this.R = R;
+class Resisteur extends Composant{
+  constructor(resistance) {
+    this.resistance = resistance;
+    this.courant = 0;
+    this.tension = 0;
+  }
+  calcul(paralele, composant) {
+    if (paralele) {
+      this.tension = composant.tension;
+      this.calculCourant();
+    } else {
+      this.courant = composant.courant;
+      this.calculTension();
     }
+    //calcul_Valeurs(paralele, this, courant, tension);
+  }
+  calculTension() {
+    this.tension = this.resistance * this.courant;
+  }
 
-    calcul(){
-        calculCourant();//mettre la tension équivalente
-    }
+  calculCourant() {
+    this.courant = this.tension / this.resistance;
+  }
 
-    calculCourant(tension_equ){
-        // Todo formule
-    }
-
+  getType() {
+    return "Résisteur";
+  }
 }
+/*function calcul_Valeurs(paralele, composant, courant, tension, charge) {
+  if (paralele) {
+    composant.tension = tension;
+    if (composant.getType() == "Résisteur") calcul_courrant_R(composant);
+    else if (composant.getType() == "Condensateur") calcul_charge_C(composant);
+    else {
+      calcul_courrant_R(composant);
+      calcul_charge_C(composant);
+    }
+  } else {
+    composant.courant = courant;
+    if (composant.getType() == "Résisteur") {
+      composant.courant = courant;
+      calcul_tension_R(composant);
+    } else if (composant.getType() == "Condensateur")
+      calcul_tension_C(composant);
+    else {
+      composant.courant = courant;
+      composant.charge = charge;
+      calcul_tension_R(composant);
+      calcul_tension_C(composant);
+    }
+  }
+}
+function calcul_courrant_R(composant) {
+  composant.courant = composant.tension / composant.resistance;
+}
+
+function calcul_tension_R(composant) {
+  composant.tension = composant.resistance * composant.courant;
+}
+function calcul_charge_C(composant) {
+  composant.charge = composant.capacite * composant.tension;
+}
+
+function calcul_tension_C(composant) {
+  composant.tension = composant.charge / composant.capacite;
+}*/
+
 class Condensateur extends Composant{
-    constructor(circuit, courant, tension, R){
-        super(circuit, courant, tension);
-        this.R = R;
+  constructor(capacite) {
+    this.capacite = capacite;
+    this.charge = 0;
+    this.tension = 0;
+    this.courant = 0;
+  }
+  calcul(paralele, composant) {
+    this.courant = composant.courant;
+    if (paralele) {
+      this.tension = composant.tension;
+      this.calculCharge();
+    } else {
+      this.charge = composant.charge;
+      this.calculTension();
     }
-    calcul(){
-
-    }
-
+    //calcul_Valeurs(paralele, this, courant, tension);
+  }
+  calculCharge() {
+    this.charge = this.capacite * this.tension;
+  }
+  calculTension() {
+    this.tension = this.charge / this.capacite;
+  }
+  getType() {
+    return "Condensateur";
+  }
 }
-class Diode extends Composants{
+
+class Diode extends Composant{
     constructor(circuit){
         super(circuit, courant, tension);
         this.R = R;
@@ -45,7 +116,7 @@ class Diode extends Composants{
 
     }
 }
-class Ampoule extends Composants{
+class Ampoule extends Composant{
     constructor(circuit){
         super(circuit, courant, tension);
         this.R = R;
@@ -54,7 +125,7 @@ class Ampoule extends Composants{
 
     }
 }
-class Batterie extends Composants{
+class Batterie extends Composant{
     constructor(){
         tension = 10;
     }
