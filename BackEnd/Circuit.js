4132c8c15458e7b6aad5359fb1d70bb6c8e988b0
Lolman255 <117114873+Lolman255@users.noreply.github.com>
2023-03-27 15:19:29 -0400

    Update Circuit.js

--- BackEnd/Circuit.js
+++ BackEnd/Circuit.js
@@ -1,13 +1,12 @@
-/**
- * La première composante devrait toujours être la pile
- */
-let circuit = [new Batterie];
+let circuit = [];
+let circuitEq = [];
 
 function ajouterComposanteALaFin(composant){
     circuit.push(composant);
 }
 
-function ajouterComposante(composant, position){
+//position est l'emplacement de la valeur a modifier, le 0 est le nombre de valeur a modifier et composant est ce qu'on ajoute à la position
+function ajouterComposante(composant, position){ 
     circuit.splice(position, 0, composant);
 }
 
@@ -36,7 +35,57 @@ function start(){ //Une fois au début
  * quand une valeur dans une composante est modifiée.
  */
 function update(){//Chaque fois qu'il y a un changement dans le circuit
-    for (let i = 0; i < circuit.length; i++){
-        circuit[i].calcul;
+    rearrangerArrayCircuit();
+    if(validerCircuit()){
+        trouverEq();
+
+        //Modifer seulement le circuit Eq
+
+        revenirCircuitDeBase();
     }
+    
+}
+
+function rearrangerArrayCircuit(){
+    //Mettre la pile début
+}
+
+function validerCircuit(){
+    //Voir si le circuit est valide
+}
+function trouverEq(){
+    //Cette méthode aura pour but de simplifier au max notre circuit de base.
+
+    circuitEq[0] = circuit[0] //Mettre la pile au début dans le circuit équivalent
+        if(enSerie()){
+            for (let i = 1; i < circuit.length; i++){ //Commence à 1 parce qu'on sait qu'il y a la pile à 0
+                let composantEq;
+                if(circuit[i].getType() == circuit[i - 1]){
+                    if(circuit.getType() == "résisteur"){
+                        
+                    } else if(circuit.getType() == "condensateur"){
+                        composantEq
+                    }
+                } else{
+                    composantEq = circuit[i];
+                }
+
+                circuitEq.push(composantEq);
+            }
+        }else{ //Il y a au moins un noeud dans le circuit (En parallèle)
+
+        }
+}
+
+/**
+ * Vérifie s'il y a un noeud dans le circuit. S'il y a un noeud, le circuit ne peut pas être en série.
+ * @returns 
+ */
+function enSerie(){
+    for (let i = 0; i < circuit.length; i++){
+        if(circuit[i].getType == "noeud"){
+            return false
+        }
+    }
+    return true;
 }
