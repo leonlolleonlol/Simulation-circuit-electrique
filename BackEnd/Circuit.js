0b4892fae8c75895d19e0b4fac4ac02dc835fe27
olivierstKronos <123409452+olivierstKronos@users.noreply.github.com>
2023-03-26 20:35:30 -0400

    Documenter notre code
    J'ai surtout fait une simplification et une explication du code qu'il reste à implémenter pour fonctionner avec le reste du code

--- BackEnd/Circuit.js
+++ BackEnd/Circuit.js
@@ -1,17 +1,31 @@
-let circuit = [new Batterie];
+//let circuit = [new Batterie];
+// Le circuit ne possède pas nécessairement une batterie
 
-function ajouterComposante(composante){
-    circuit.push(composante);
-}
+class Circuit{
+    constructor(){
+        this.composants = [];// Toutes des branches puisque le circuit est en fait plusieurs circuit ou chaque circuit est représenter par une branche
+        
+    }
+    ajouterComposante(composante){
+        this.composants.push(composante);
+    }
 
-function retirerComposante(position){
+    retirerComposante(position){
+        
+    }
     
+    connectComposants(Composant1,composant2){
+        // C'est ici que les branches vont se rassembler et que la logique de construction de circuit se pass
+        // 1. Trouver la branche des deux composants
+        // 2. Faire une fusion des deux branches
+    }
+    start(){ 
+        //Une fois au début
+    }
+
+    update(temps){
+        //Chaque changement dans le circuit
+    }
 }
 
-function start(){ //Une fois au début
 
-}
-
-function update(){//Chaque changement dans le circuit
-
-}
