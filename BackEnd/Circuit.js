07efcce93000b092df88e6df53a37137260478ca
Lolman255 <stban.renaud@gmail.com>
2023-03-28 11:02:28 -0400

    Fusion de l'idée de groupe d'Olivier
    Les groupes sont devenu la class circuit.
    Les groupebranches sont devenu les noeuds

--- BackEnd/Circuit.js
+++ BackEnd/Circuit.js
@@ -1,107 +1,139 @@
-let circuit = [];
-let circuitEq = [];
-
-function ajouterComposanteALaFin(composant){
-    circuit.push(composant);
-}
-
-//position est l'emplacement de la valeur a modifier, le 0 est le nombre de valeur a modifier et composant est ce qu'on ajoute à la position
-function ajouterComposante(composant, position){ 
-    circuit.splice(position, 0, composant);
-}
-
-function retirerComposante(position){
-    circuit.splice(position, 1);
-}
-
-function echangerComposantes(position1, position2){
-    let temp1 = circuit[position1];
-    let temp2 = circuit[position2];
-    if(position1 < position2){
-        circuit.splice(position1, 1, temp2);
-        circuit.splice(position2, 1, temp1);
-    } else if(position2 < position1){
-        circuit.splice(position2, 1, temp1);
-        circuit.splice(position1, 1, temp2);
-    }
-}
-
-function start(){ //Une fois au début
-
-}
-
 /**
- * Doit se faire appeler quand il y a une nouvelle connection entre 2 composantes, quand une connection est brisée et
- * quand une valeur dans une composante est modifiée.
+ * Contient une suite de composantes en série. S'il y a un noeud, 
+ * c'est dans le noeud qu'on retrouvera les circuits en parallèle.
  */
-function update(){//Chaque fois qu'il y a un changement dans le circuit
-    rearrangerArrayCircuit();
-    if(validerCircuit()){
-        trouverEq();
+class Circuit{
+    /**
+     * Initialise toute ce qu'on aura besoin pour les calculs. Si c'est une branche, la variable circuit devient "children" 
+     * @param {boolean} premierCircuit cette variable sert à indiquer si c'est cet objet qui contient le circuit au complet.
+     * Donc c'est true si ce n'est pas une branche du circuit construit par l'utilisateur.
+     */
+    constructor(premierCircuit){
+        this.premierCircuit = premierCircuit;
+        this.circuit = []; //Array de composantes
+        this.valide = true;
 
-        let SansCond = true;
-        let iEq = 0;
-        for (let i = 0; i < circuit.length; i++){ //Commence à 1 parce qu'on sait qu'il y a la pile à 0
-            if(circuitEq[i].getType == "condensateur"){
-                SansCond = false;
-            }
-        }
-        if(SansCond){
-            iEq = courantSansCondensateur();
-            //for ... de circuit
-            // circuit[].i = ieq  problème avec circuit parallèle
-        }
+        //C'est variable sont utile dans le cas où l'instance de cette class est une branche
+        this.capaciteEQ = 0;
+        this.resistanceEQ = 0;
+        
+        //Sert stocker le type de circuit. AKA -> seulement des résistances, seulement des condensateurs ou RC.
+        this.type;
+    }
 
-        //Trouver comment backtrack
-         
+    ajouterComposanteALaFin(composant){
+        circuit.push(composant);
     }
     
-}
-
-function rearrangerArrayCircuit(){
-    //Mettre la pile début
-}
-
-function validerCircuit(){
-    //Voir si le circuit est valide
-}
-function trouverEq(){
-    //Cette méthode aura pour but de simplifier au max notre circuit de base.
-
-    circuitEq[0] = circuit[0] //Mettre la pile au début dans le circuit équivalent
-        if(enSerie()){
-            for (let i = 1; i < circuit.length; i++){ //Commence à 1 parce qu'on sait qu'il y a la pile à 0
-                let composantEq;
-                if(circuit[i].getType() == circuit[i - 1]){
-                    if(circuit.getType() == "résisteur"){
-                        
-                    } else if(circuit.getType() == "condensateur"){
-                        composantEq
-                    }
-                } else{
-                    composantEq = circuit[i];
-                }
-
-                circuitEq.push(composantEq);
-            }
-        }else{ //Il y a au moins un noeud dans le circuit (En parallèle)
-
-        }
-}
-function courantSansCondensateur() {
-    // Trouver Courant avec delta V de la pile et Req
-    return circuitEq[0].tension * circuitEq.R;
-}
-
-/**
- * Vérifie s'il y a un noeud dans le circuit. S'il y a un noeud, le circuit ne peut pas être en série.
- * @returns 
- */
-function enSerie(){
-    for (let i = 0; i < circuit.length; i++){
-        if(circuit[i].getType == "noeud"){
-            return false
+    //position est l'emplacement de la valeur a modifier, le 0 est le nombre de valeur a modifier et composant est ce qu'on ajoute à la position
+    ajouterComposante(composant, position){ 
+        circuit.splice(position, 0, composant);
+    }
+    
+    retirerComposante(position){
+        circuit.splice(position, 1);
+    }
+    
+    echangerComposantes(position1, position2){
+        let temp1 = circuit[position1];
+        let temp2 = circuit[position2];
+        if(position1 < position2){
+            circuit.splice(position1, 1, temp2);
+            circuit.splice(position2, 1, temp1);
+        } else if(position2 < position1){
+            circuit.splice(position2, 1, temp1);
+            circuit.splice(position1, 1, temp2);
         }
     }
-    return true;
+  
+    /**
+     * Doit se faire appeler quand il y a une nouvelle connection entre 2 composantes, quand une connection est brisée et
+     * quand une valeur dans une composante est modifiée. Elle doit aussi seulement être appellé une fois au début sur la
+     * branche principale.
+     */
+    update(){//Chaque fois qu'il y a un changement dans le circuit
+        if(this.premierCircuit){
+            rearrangerArrayCircuit();
+        }
+        validerCircuit()
+
+        if(this.valide){
+            trouverEq();
+        }
+    
+    }
+    
+    /**
+     * Change l'ordre des composantes pour que la pile soit à position 0 dans circuit.
+     */
+    rearrangerArrayCircuit(){
+        
+    }
+    
+    /**
+     * Regarde s'il y a au moins un chemin possible pour le courant
+     */
+    validerCircuit(){
+        this.valide = true;
+    }
+
+    /**
+     * Sert à trouver le circuit équivalent en série
+     */
+    trouverEq(){
+        trouverTypeDeCircuit();
+
+        switch (this.type){
+            case "seulementR":
+                for (let i = 0; i < circuit.length; i++){
+                    if(this.circuit[i].getType == "Noeud"){
+                        this.resistanceEQ += this.circuit[i].resistanceEQ;
+                    }else{
+                        this.resistanceEQ += this.circuit[i].resistance;
+                    }
+                }
+                break;
+            case "seulementC":
+                let capaciteTemp = 0;
+                for (let i = 0; i < circuit.length; i++){ 
+                    capaciteTemp += 1 / this.circuit[i];
+                }
+                this.capaciteEQ = 1/capaciteTemp;
+                break;
+            case "RC":
+                //C'est là que c'est difficile
+                break;
+        }
+    }
+
+    trouverTypeDeCircuit(){
+        let circuitR = false;
+        let circuitC = false;
+        for (let i = 0; i < circuit.length; i++){ 
+            switch(circuit[i].getType()){
+                case "Ampoule": //Une ampoule agie comme une résistance
+                case "Résisteur":
+                    circuitR = true;
+                    break;
+                case "Condensateur":
+                    circuitC = true;
+                    break;
+                case "Noeud":
+                    circuit[i].trouverEq;
+                    break;
+            }
+        }
+
+        if(circuitR && circuitC){
+            this.type = "RC"
+        }else if (circuitC){
+            this.type = "seulementC"
+        }else{
+            this.type = "seulementR"
+        }
+    }
+
+    getType(){
+        return this.type;
+    }
 }
--- BackEnd/Composants.js
+++ BackEnd/Composants.js
@@ -1,73 +1,216 @@
 class Composant {
 
-    constructor(circuit, courant, tension){
-        // on crée automatiquement une classe de tension
-        this.circuit = circuit;
-        this.courant = courant;
-        this.tension = tension;
-    }
-    calcul(){
-        throw console.error();
-    }
+  constructor(courant, tension){
+      // on crée automatiquement une classe de tension
+      this.courant = courant;
+      this.tension = tension;
+  }
+  // Cette méthode est appelé à chaque fois que l'on
+  // veut calculer la tension et la différence de pottentiel d'un 
+  // composant. Les information disponible sont la branche (composant) et 
+  //si notre composant est en paralèle ou en série
+  calcul(paralele, composant){
+      throw console.error();//todo préciser l'erreur
+  }
+  getType() {
+    throw console.error();//todo préciser l'erreur
+  }
 } 
 
-class Resistance extends Composant{
-    constructor(circuit, courant, tension, R){
-        super(circuit, courant, tension);
-        this.R = R;
+class Resisteur{
+  constructor(resistance) {
+    this.resistance = resistance;
+    this.courant = 0;
+    this.tension = 0;
+  }
+  calcul(paralele, composant) {
+    print(this);
+    if (paralele) {
+      this.tension = composant.tension;
+      this.calculCourant();
+    } else {
+      this.courant = composant.courant;
+      this.calculTension();
     }
-
-    calcul(){
-        calculCourant();//mettre la tension équivalente
-    }
-
-    calculCourant(tension_equ){
-        // Todo formule
-    }
-
+    //calcul_Valeurs(paralele, this, courant, tension);
+  }
+  getType() {
+    return "Résisteur";
+  }
 }
-class Condensateur extends Composant{
-    constructor(circuit, courant, tension, R){
-        super(circuit, courant, tension);
-        this.R = R;
-    }
-    calcul(){
 
+class Condensateur{
+  constructor(capacite) {
+    this.capacite = capacite;
+    this.charge = 0;
+    this.tension = 0;
+    this.courant = 0;
+  }
+  calcul(paralele, composant) {
+    this.courant = composant.courant;
+    if (paralele) {
+      this.tension = composant.tension;
+      this.calculCharge();
+    } else {
+      this.charge = composant.charge;
+      this.calculTension();
     }
-
+    //calcul_Valeurs(paralele, this, courant, tension);
+  }
+  calculCharge() {
+    this.charge = this.capacite * this.tension;
+  }
+  calculTension() {
+    this.tension = this.charge / this.capacite;
+  }
+  getType() {
+    return "Condensateur";
+  }
 }
-class Diode extends Composants{
-    constructor(circuit){
-        super(circuit, courant, tension);
-        this.R = R;
-    }
-    calcul(){
 
-    }
+class Diode extends Composant{
+  constructor(circuit){
+      super(circuit, courant, tension);
+      this.R = R;
+  }
+  calcul(){
+
+  }
 }
-class Ampoule extends Composants{
-    constructor(circuit){
-        super(circuit, courant, tension);
-        this.R = R;
-    }
-    calcul(){
+class Ampoule extends Composant{
+  constructor(circuit){
+      super(circuit, courant, tension);
+      this.R = R;
+  }
+  calcul(){
 
-    }
+  }
 }
-class Batterie extends Composants{
-    constructor(){
-        tension = 10;
-    }
-    calcul(){
+class Batterie extends Composant{
+  constructor(){
+      tension = 10;
+  }
+  calcul(){
 
-    }
+  }
 
-    setTension(x){
-        this.tension = x;
-    }
+  setTension(x){
+      this.tension = x;
+  }
 }
+
+
 class Switch extends Composant {
+
 }
+
+/**
+ * C'est dans cette class qu'on va faire les calculs pour les circuits en parallèle.
+ */
 class Noeuds extends Composant {
+  constructor(courant, tension){
+    super(courant, tension)
+    this.circuitsEnParallele = []; //Array de Circuit
+
+    //C'est variable sont utile dans le cas où l'instance de cette class est une branche
+    this.capaciteEQ = 0;
+    this.resistanceEQ = 0;
+  }
+ /**
+  * Sert à trouver le circuit équivalent en série
+  */
+ trouverEq(){
+    for (let i = 0; i < circuitsEnParallele.length; i++){ 
+      this.circuitsEnParallele[i].trouverEq();
+    }
+
+    let typeDeCircuit = this.trouverTypeDeCircuit();
+
+    
+    switch (typeDeCircuit){
+        case "seulementR":
+          let resistancetemp = 0;
+            for (let i = 0; i < circuitsEnParallele.length; i++){ 
+                resistancetemp += 1 / this.circuit[i].resistanceEQ;
+            }
+            this.resistanceEQ = 1 / resistanceTemp;
+            break;
+        case "seulementC":
+            for (let i = 0; i < circuitsEnParallele.length; i++){ 
+              this.capaciteEQ += this.circuit[i];
+            }
+            break;
+        case "RC":
+            //C'est là que c'est difficile
+            break;
+    }
+  }
+
+  trouverTypeDeCircuit(){
+    let circuitR = false;
+    let circuitC = false;
+
+    for (let i = 0; i < circuitsEnParallele.length; i++){ 
+        switch(circuitsEnParallele[i].getType()){
+            case "seulementR":
+                circuitR = true;
+                break;
+            case "seulementC":
+                circuitC = true;
+                break;
+        }
+    }
+
+    if(circuitR && circuitC){
+        this.type = "RC"
+    }else if (circuitC){
+        this.type = "seulementC"
+    }else{
+        this.type = "seulementR"
+    }
+  }
+
+  getType() {
+    return "Noeud";
+  }
 }
+
+/*function calcul_Valeurs(paralele, composant, courant, tension, charge) {
+if (paralele) {
+  composant.tension = tension;
+  if (composant.getType() == "Résisteur") calcul_courrant_R(composant);
+  else if (composant.getType() == "Condensateur") calcul_charge_C(composant);
+  else {
+    calcul_courrant_R(composant);
+    calcul_charge_C(composant);
+  }
+} else {
+  composant.courant = courant;
+  if (composant.getType() == "Résisteur") {
+    composant.courant = courant;
+    calcul_tension_R(composant);
+  } else if (composant.getType() == "Condensateur")
+    calcul_tension_C(composant);
+  else {
+    composant.courant = courant;
+    composant.charge = charge;
+    calcul_tension_R(composant);
+    calcul_tension_C(composant);
+  }
+}
+}
+function calcul_courrant_R(composant) {
+composant.courant = composant.tension / composant.resistance;
+}
+
+function calcul_tension_R(composant) {
+composant.tension = composant.resistance * composant.courant;
+}
+function calcul_charge_C(composant) {
+composant.charge = composant.capacite * composant.tension;
+}
+
+function calcul_tension_C(composant) {
+composant.tension = composant.charge / composant.capacite;
+}*/
 
