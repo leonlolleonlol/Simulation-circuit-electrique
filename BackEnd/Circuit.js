b74f8b1115189ab2407cec3a0b3b888504b19f45
AxelShami <123408991+AxelShami@users.noreply.github.com>
2023-04-17 15:46:03 -0400

    Update Circuit.js

--- BackEnd/Circuit.js
+++ BackEnd/Circuit.js
@@ -63,12 +63,26 @@
         this.trouverEq();
         
     }
-    
     /**
      * Change l'array pour que le circuit soit en série grâce à l'historique. Après cette méthode, le
      * circuit devrait commencer de la pile, puis finir à la composante juste avant la pile.
      */
     rearrangerArrayCircuit(){
+        let index
+        for(){
+            /**apartir de la pile on retrace le chemin en utilisant les fil pour définir la prochaine com*/
+          this.circuit[index++] 
+           if(true){//le fil donne lieu à 2 différents fils
+            //étape 1 : fil 1 =
+            /** on se retouvre dans un noeud*/
+            while(this.circuit[index]){
+                index++
+            }
+        }else{index++}
+        }
+             
+       
+        
     }
 
     /**
