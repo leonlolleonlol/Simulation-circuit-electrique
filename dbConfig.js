b3e23e8babfd0047a2632bf8b3f9690c0903b4d0
leonlolleonlol <leonkoj2004@gmail.com>
2023-05-13 17:18:00 -0400

    testUserAuth all good!

--- dbConfig.js
+++ dbConfig.js
@@ -4,10 +4,10 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgres://db_for_users_user:HyhtdJvzf3LMiJ2CtRW0gIjfTomNakJw@dpg-ch9fhjhmbg52oq6aq81g-a.oregon-postgres.render.com/db_for_users?ssl=true`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: /*isProduction ? process.env.DATABASE_URL :*/ connectionString,
   ssl: isProduction
 });
 
